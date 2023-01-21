b67258f6dba7a1c7f83849edc2296639606109fd
Jhon Ponte <batmanponte@outlook.com>
2023-01-20 22:13:50 -0500

    agroingenieria

--- src/componentes/ItemsCards_Texto_NuestrosServicios.jsx
+++ src/componentes/ItemsCards_Texto_NuestrosServicios.jsx
@@ -5,10 +5,11 @@ import { BsFillDropletFill } from  'react-icons/bs'
 import { GiPencilRuler } from  'react-icons/gi'
 import { AiTwotoneContainer } from  'react-icons/ai'
 
-import { Button } from 'react-bootstrap';
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import imagen1 from  '../imagen/Agronomia-cultivo-riegotecnificado.jpg'
+import imagen2 from  '../imagen/Riego-Tecnificado1.jpg'
+import imagen3 from  '../imagen/Topografia-Superficial.jpg'
+import imagen4 from  '../imagen/HABILITACION-URBANA1.jpg'
+import imagen5 from  '../imagen/CONSTRUCCION-CIVIL.jpg'
 
 import '../css/NuestroServicio.css'
 import '../css/imagenprincipal.css'
@@ -39,7 +40,8 @@ export default function ItemsCards_TextoCircular() {
 
               <div class="order-3 card p-2 colorfondocard text-white border-0 rounded-0 col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
 
-              <img class="img-fluid redondearimagen border-dark text-center" src="https://i.postimg.cc/4xP74R77/Agronomia-cultivo-riegotecnificado.jpg" alt="Card image cap"/>
+              {/* <img class="img-fluid redondearimagen border-dark text-center" src="https://i.postimg.cc/4xP74R77/Agronomia-cultivo-riegotecnificado.jpg" alt="Card image cap"/> */}
+              <img class="img-fluid redondearimagen border-dark text-center" src={imagen1} alt="Card image cap"/>
               
                 <div class="card-body ColorFondoCard">
                   <h5 class="card-title shadow-lg p-3 mb-5 rounded shadow-text titulo2-text">INGENIERIA AGRICOLA</h5>
@@ -56,7 +58,9 @@ export default function ItemsCards_TextoCircular() {
               {/* ----2  */}
               {/* col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4  */}
               <div class="order-4 card p-2 colorfondocard text-white border-0 rounded-0 col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
-                  <img class="img-fluid redondearimagen border-5 border-dark" src="https://i.postimg.cc/50DgGWqG/Riego-Tecnificado1.jpg" with="50%" height="30%" alt="Card image cap"/>
+                  {/* <img class="img-fluid redondearimagen border-5 border-dark" src="https://i.postimg.cc/50DgGWqG/Riego-Tecnificado1.jpg" with="50%" height="30%" alt="Card image cap"/> */}
+                  <img class="img-fluid redondearimagen border-5 border-dark" src={imagen2} with="50%" height="30%" alt="Card image cap"/>
+                  
                   <div class="card-body">
                     <h5 class="card-title shadow-lg p-3 mb-5 rounded shadow-text titulo2-text">RIEGO TECNIFICADO EN EL SEMBRIO</h5>
                     <p class="card-text titulo4-text shadow-text texto-justificado">
@@ -72,7 +76,9 @@ export default function ItemsCards_TextoCircular() {
 
               {/* ----3  */}
               <div class="order-1 card p-2 colorfondocard text-white border-0 rounded-0 col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
-                <img class="img-fluid redondearimagen border-5 border-dark" src="https://i.postimg.cc/505vdQVv/Topografia-Superficial.jpg" with="50%" height="30%" alt="Card image cap"/>                
+                {/* <img class="img-fluid redondearimagen border-5 border-dark" src="https://i.postimg.cc/505vdQVv/Topografia-Superficial.jpg" with="50%" height="30%" alt="Card image cap"/>                 */}
+                <img class="img-fluid redondearimagen border-5 border-dark" src={imagen3} with="50%" height="30%" alt="Card image cap"/>                
+
                 <div class="card-body">
                   <h5 class="card-title shadow-lg p-3 mb-5 rounded shadow-text titulo2-text">TOPOGRAFIA, GEODESIA</h5>
                   <p class="card-text titulo4-text shadow-text texto-justificado">                
@@ -88,7 +94,8 @@ export default function ItemsCards_TextoCircular() {
                 {/* ---- 4  */}                
                 {/* col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 */}
                 <div class="order-2 card p-2 colorfondocard text-white border-0 rounded-0 col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
-                  <img class="img-fluid redondearimagen border-5 border-dark" src="https://i.postimg.cc/MHkVfk77/HABILITACION-URBANA1.jpg" with="50%" height="30%" alt="Card image cap"/>                  
+                  {/* <img class="img-fluid redondearimagen border-5 border-dark" src="https://i.postimg.cc/MHkVfk77/HABILITACION-URBANA1.jpg" with="50%" height="30%" alt="Card image cap"/>                   */}
+                  <img class="img-fluid redondearimagen border-5 border-dark" src= {imagen4} with="50%" height="30%" alt="Card image cap"/>                  
                   <div class="card-body">
                     <h5 class="card-title shadow-lg p-3 mb-5 rounded shadow-text titulo2-text">HABILITACION URBANA Y CATASTRO</h5>
                     <p class="card-text titulo4-text shadow-text texto-justificado">                                  
@@ -105,7 +112,8 @@ export default function ItemsCards_TextoCircular() {
 
                 {/* ----5  */}     
                 <div class="order-5 card p-2 colorfondocard text-white border-0 rounded-0 col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
-                  <img class="img-fluid redondearimagen border border-5 border-dark" src="https://i.postimg.cc/wjDqYrrc/CONSTRUCCION-CIVIL.jpg" with="50%" height="30%" alt="Card image cap"/>                  
+                  {/* <img class="img-fluid redondearimagen border border-5 border-dark" src="https://i.postimg.cc/wjDqYrrc/CONSTRUCCION-CIVIL.jpg" with="50%" height="30%" alt="Card image cap"/>                   */}
+                  <img class="img-fluid redondearimagen border border-5 border-dark" src={imagen5} with="50%" height="30%" alt="Card image cap"/>                  
 
                   <div class="card-body">
                     <h5 class="card-title shadow-lg p-3 mb-5 rounded shadow-text titulo2-text">CONSTRUCCIONES Y OBRAS CIVILES</h5>
