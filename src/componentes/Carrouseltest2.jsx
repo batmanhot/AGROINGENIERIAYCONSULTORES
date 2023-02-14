82f82b079dc9cc3631a6b196cb7ee4eeb6a5bc84
Jhon Ponte <batmanponte@outlook.com>
2023-02-13 23:06:47 -0500

    agroingenieria

--- src/componentes/Carrouseltest2.jsx
+++ src/componentes/Carrouseltest2.jsx
@@ -1,5 +1,9 @@
 
 import React, { Component } from 'react';
+import '../css/carrousel.css'
+import '../css/NuestroServicio.css'
+import '../css/imagenprincipal.css'
+
 import {
   Carousel,
   CarouselItem,
@@ -119,25 +123,38 @@ class Example extends Component {
         //    </div>
 
         >          
-         <div class='row'>
-            <div class="container-fluid">
-              <div className="col-xs-3 col-sm-4 col-md-8 col-lg-8 col-xl-12">
+         {/* <div class='container'> */}
+            <div class="row">
+              {/* <div className="col-xs-10 col-sm-12 col-md-12 col-lg-12 col-xl-12"> */}
                   <div className="card bg-dark text-white rounded-0">
-                      <div className="card-body col-xs-3 col-sm-4 col-md-8 col-lg-8 col-xl-12">
+                      <div className="card-body">
                       {/* width = "700rem" height="500rem" */}
                       {/* <img class="img-fluid redondearimagen border-dark text-center" src="https://i.postimg.cc/4xP74R77/Agronomia-cultivo-riegotecnificado.jpg" alt="Card image cap"/> */}
-                        {/* <img class="img-fluid" src={item.src} alt={item.altText} width = "830px" height="490px"/> */}
-                        <img  src={item.src} alt={item.altText} width = "830px" height="550px"/>
-                        <CarouselCaption captionText={item.caption_2} captionHeader={item.caption} /> 
+                        {/* 830 * 490 */}
+                        {/* <img src={item.src} alt={item.altText} width = "550px" height="650px" className="col-xs-10 col-sm-10 col-md-10 col-lg-8 col-xl-8"/> */}
+                        {/* <img src={item.src} alt={item.altText} width = "520rem" height="40rem"/> */}
+                          <center>
+                            <img class="d-flex fluid w-60 border border-3 border-white col-xs-12 col-sm-12 col-md-10 col-lg-8 col-xl-8" 
+                             src={item.src} 
+                             alt={item.altText}
+                             height="650px"/>
+                          </center>
+
+                          <div className="card-text">                             
+                            {/* <p class="titulo2-text">{item.caption_2}</p>
+                            <p class="titulo2-text">{item.caption}</p>  */}
+                            <CarouselCaption className="demo-carousel" captionText={item.caption_2} captionHeader={item.caption} /> 
+                            
+                          
+                          </div>
                       </div>
-                  </div>
-              </div>
+                  </div>                               
             </div>
-          </div>
+          {/* </div> */}
 
           {/* <img src={item.src} alt={item.altText} /> */}
           {/* <CarouselCaption captionText={item.altText} captionHeader={item.caption} /> */}
-        </CarouselItem>
+          </CarouselItem>
       );
     });
 
